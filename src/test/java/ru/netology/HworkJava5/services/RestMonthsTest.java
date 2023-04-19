a1167cd276e9bf4d02a2ecd07660fa6a83bc47ce
Dmitry Boychevsky <javashdrug@yandex.ru>
2023-04-19 15:23:07 +0300

    Task 2

--- src/test/java/ru/netology/HworkJava5/services/RestMonthsTest.java
+++ src/test/java/ru/netology/HworkJava5/services/RestMonthsTest.java
@@ -1,32 +1,33 @@
 package ru.netology.HworkJava5.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestMonthsTest {
-    @Test
-    public void RestMonthsService() {
+    @ParameterizedTest
+    @CsvFileSource(files ="src/test/resources/RestMonths.csv")
+    public void RestMonthsService(int income, int expenses, int threshold, int expected) {
         RestMonthsService service = new RestMonthsService();
-
-        int income = 10_000;
-        int expenses = 3000;
-        int threshold = 20_000;
-        int expected = 3;
-
+//        int income = 10_000;
+//        int expenses = 3000;
+//        int threshold = 20_000;
+//        int expected = 3;
         int actual = service.calculate(income, expenses, threshold); // вызываем целевой метод
         Assertions.assertEquals(expected, actual); // производим проверку (сравниваем ожидаемый и фактический)
 
     }
 
-    @Test public void RestMonthsService2() {
-        RestMonthsService service = new RestMonthsService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-
-        int actual = service.calculate(income, expenses, threshold);// вызываем целевой метод:
-        Assertions.assertEquals(expected, actual); // производим проверку (сравниваем ожидаемый и фактический)
-
-    }
+//    @Test public void RestMonthsService2() {
+//        RestMonthsService service = new RestMonthsService();
+//
+//        int income = 100_000;
+//        int expenses = 60_000;
+//        int threshold = 150_000;
+//        int expected = 2;
+//
+//        int actual = service.calculate(income, expenses, threshold);// вызываем целевой метод:
+//        Assertions.assertEquals(expected, actual); // производим проверку (сравниваем ожидаемый и фактический)
+//
+//    }
 }
